220b2a3b6e3e1ba5635a47088c0606893bfd9851
Meghabetageri <meghabetageri126@gmail.com>
2025-02-09 01:32:46 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-05fa46471b02db0ce"  # Replace with a valid AMI ID for your region
+  ami           = "ami-00bb6a80f01f03502"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"  # Instance type
   
   tags = {
